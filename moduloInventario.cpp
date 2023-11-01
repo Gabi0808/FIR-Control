848287b64cf8f3262c3f9834ee8423158b64bd96
Andres <amartinezs@uamv.edu.ni>
2023-11-01 08:16:07 -0600

    cambios en modulo inventario

--- moduloInventario.cpp
+++ moduloInventario.cpp
@@ -10,25 +10,58 @@ typedef struct
     string nombreProducto;
     float precioProducto;
     int cantidadProducto;
-} producto;
+} Producto;
 
-int main(){
-producto inventarioProductos[] = {
-    {"081501", "Victoria Clasica Lata 355mL", 45, 100}};
+int main()
+{
+    Producto inventarioProductos[] = {
+        {"081501", "Victoria Clasica Lata 355mL", 45, 100}};
 
-ofstream archivo("inventario.bin", ios::binary);
+    ofstream archivo("inventario.bin", ios::binary);
 
- if (archivo.is_open()) {
-        for (const producto &producto : inventarioProductos) {
-            archivo.write(reinterpret_cast<const char*>(&producto), sizeof(producto));
+    if (archivo.is_open())
+    {
+        for (const Producto &producto : inventarioProductos)
+        {
+            archivo.write(reinterpret_cast<const char *>(&producto), sizeof(producto));
         }
 
         archivo.close();
 
         cout << "El inventario se ha guardado en el archivo 'inventario.bin'." << endl;
-    } else {
-        cerr << "No se pudo abrir el archivo." << endl;
     }
+    else
+    {
+        cerr << "No se pudo abrir el archivo." << endl;
+    };
 
-    return 0;
+    void mostrarDatosBinarios(const string &nombreArchivo)
+    {
+        ifstream archivo(nombreArchivo, ios::binary);
+        if (archivo.is_open())
+        {
+            Producto producto;
+            while (archivo.read(reinterpret_cast<const char *>)(&producto), sizeof(producto))
+            {
+                cout << "Codigo: " << producto.codigoProducto << endl;
+                cout << "Nombre: " << producto.nombreProducto << endl;
+                cout << "Precio: " << producto.precioProducto << endl;
+                cout << "Cantidad: " << producto.cantidadProducto << endl;
+                cout << "------------------------------" << endl;
+            }
+
+            archivo.close();
+        }
+        else
+        {
+            cerr << "No se pudo abrir el archivo. " << endl;
+        }
+    }
+    int mostrarDatos()
+    {
+        const string nombreArchivo = "inventario.bin";
+        mostrarDatosBinarios(nombreArchivo);
+
+        return 0;
+    }
 }
